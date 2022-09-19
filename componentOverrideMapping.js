b73ca9be1fc804b2286fbd15c5993af571b51dd7
hieptvAHT <hieptv1998@gmail.com>
2022-09-19 19:31:06 +0700

    update

---
+++ componentOverrideMapping.js
@@ -0,0 +1,9 @@
+module.exports = componentOverride = {
+    [`@magento/venia-ui/lib/components/ErrorView`]: 'src/components/ErrorView',
+    [`@magento/venia-ui/lib/components/LinkButton`]: 'src/components/LinkButton',
+    [`@magento/peregrine/lib/talons/WishlistPage/wishlistItemFragments.gql.js`]: 'src/components/Wishlist/Queries/wishlistItemFragments.gql.js',
+    [`@magento/venia-ui/lib/components/Wishlist/AddToListButton`]: 'src/components/Wishlist/AddToListButton',
+    [`@magento/peregrine/lib/talons/AccountMenu/useAccountMenuItems.js`]: 'src/components/AccountMenu/useAccountMenuItems.js',
+    [`@magento/venia-ui/lib/components/MiniCart/ProductList/item.js`]: 'src/components/MiniCart/ProductList/item.js',
+    [`@magento/peregrine/lib/talons/WishlistPage/wishlistItem.gql.js`]: 'src/components/Wishlist/Queries/wishlistItem.gql.js',
+};
