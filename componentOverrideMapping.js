fb740371114f2b343af33bbdb03da9f67aa0ae47
sucwin <taipv@arrowhitech.com>
2022-09-20 18:59:11 +0700

    make ui: sign in

--- componentOverrideMapping.js
+++ componentOverrideMapping.js
@@ -2,4 +2,6 @@ module.exports = componentOverride = {
     [`@magento/venia-ui/lib/components/Header`]: 'src/components/Header',
     [`@magento/venia-ui/lib/components/Main`]: 'src/components/Layout',
     [`@magento/venia-ui/lib/components/Footer`]: 'src/components/Footer',
+    [`@magento/venia-ui/lib/components/SignIn/signIn.js`]:    'src/components/SignIn/signIn.js',
+    [`@magento/venia-ui/lib/components/AccountMenu/accountMenu.js`]:    'src/components/AccountMenu/accountMenu.js',
 };
