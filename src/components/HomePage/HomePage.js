8d7c72937b96ed2efb201d8281b5e5e58dd78ebe
diego <doandtvp@gmail.com>
2022-09-20 19:41:13 +0700

    remove component

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -21,17 +21,6 @@ const HomePage = () => {
       <h1 style={hi}>Hello, {who}!</h1>
       <h1 style={wave}>{"\uD83D\uDC4B"}</h1>
       <PopupChangeAddress />
-      <ProductItemSelect
-        itemName="Ti vi cuc net"
-        itemPrice={300000000}
-      />
-      <ProductItemsRecentlyView
-        totalRate={5}
-        price={4000000000}
-        currentRate={3}
-      />
-      <ProductItemsSuggest itemName={"may giat cuc manh"} itemCode={"XFFF335F"} itemPrice={300000000} isSale={true} salePercent={20} />
-      <SelectSaleType isSelect={true} salePercent={30} orderPrice={20000} exprieDate={"20/20/2002"} exprieTime={"10:00"} listTag={[]} />
     </div>
   );
 };
