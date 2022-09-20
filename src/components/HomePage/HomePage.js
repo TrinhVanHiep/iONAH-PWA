c7e8fc21efe3a7b345642adc7d3c012c266f8ce3
diego <doandtvp@gmail.com>
2022-09-20 19:00:23 +0700

    fix module css

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import PopupChangeAddress from "../PopupChangeAddress/index"
+import ProductItemSelect from "../ProductItemSelect/ProductItemSelect"
+import ProductItemsRecentlyView from "../ProductItemsRecentlyView/ProductItemsRecentlyView"
 const hi = {
   textAlign: "center",
   margin: "1rem",
@@ -16,7 +18,16 @@ const HomePage = () => {
     <div>
       <h1 style={hi}>Hello, {who}!</h1>
       <h1 style={wave}>{"\uD83D\uDC4B"}</h1>
-      <PopupChangeAddress/>
+      <PopupChangeAddress />
+      <ProductItemSelect
+        itemName="Ti vi cuc net"
+        itemPrice={300000000}
+      />
+      <ProductItemsRecentlyView
+        totalRate={5}
+        price={4000000000}
+        currentRate={3}
+      />
     </div>
   );
 };
