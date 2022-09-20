567a34468f0e9bbf9f469bc237ce3f83ea82dbf8
diego <doandtvp@gmail.com>
2022-09-20 20:58:25 +0700

    fix conflict

--- componentOverrideMapping.js
+++ componentOverrideMapping.js
@@ -1,8 +1,9 @@
 module.exports = componentOverride = {
-    // [`@magento/venia-ui/lib/components/Header`]: 'src/components/Header',
-    // [`@magento/venia-ui/lib/components/Main`]: 'src/components/Main',
-    // [`@magento/venia-ui/lib/components/Footer`]: 'src/components/Footer',
-    [`@magento/venia-ui/lib/components/SignIn/signIn.js`]:    'src/components/SignIn/signIn.js',
-    [`@magento/venia-ui/lib/components/AccountMenu/accountMenu.js`]:    'src/components/AccountMenu/accountMenu.js',
-    [`@magento/venia-ui/lib/components/CreateAccount/createAccount.js`]:    'src/components/CreateAccount/createAccount.js',
+    [`@magento/venia-ui/lib/components/SignIn/signIn.js`]: 'src/components/SignIn/signIn.js',
+    [`@magento/venia-ui/lib/components/AccountMenu/accountMenu.js`]: 'src/components/AccountMenu/accountMenu.js',
+    [`@magento/venia-ui/lib/components/CreateAccount/createAccount.js`]: 'src/components/CreateAccount/createAccount.js',
+    [`@magento/venia-ui/lib/components/Header`]: 'src/components/Header',
+    [`@magento/venia-ui/lib/components/Main`]: 'src/components/Layout',
+    [`@magento/venia-ui/lib/components/Footer`]: 'src/components/Footer',
+    [`@magento/venia-ui/lib/components/AccountInformationPage`]: 'src/components/AccountInformationPage',
 };
--- src/components/AccountInformationPage/index.js
+++ src/components/AccountInformationPage/index.js
@@ -1,1 +1,1 @@
-export { default } from "./AccountInformationPage"
+export { default } from './accountInformationPage';
